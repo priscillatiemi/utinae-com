87847d32f4b2fc900171ce3c052724d147b75408
Priscilla Kawakami <prips.tiemi@gmail.com>
2024-10-30 19:48:35 -0300

    inserção de tela de sign-up, deslogar e urls

--- userauths/views.py
+++ userauths/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 def register_view(request):
 
@@ -25,3 +28,38 @@ def register_view(request):
         'form': form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"Usuário já está logado")
+        return redirect("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"O email {email} não existe")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login feito com sucesso!")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "Usuário não existe. Crie uma conta")
+    
+    context = {
+
+    }
+    return render(request, "userauths/sign-in.html", context)
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Sign out feito com sucesso!")
+
+    return redirect("userauths:sign-in")
+
